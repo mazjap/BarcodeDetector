bb4d28a23f1270aa897110943d4ed3457a2f7c79
Jordan Christensen <jordan.c4922@gmail.com>
2023-01-27 23:37:03 -0700

    Initial Commit

---
+++ BarCodeDetector/BarCodeDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeDetectorApp.swift
+//  BarCodeDetector
+//
+//  Created by Jordan Christensen on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
